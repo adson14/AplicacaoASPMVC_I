7baffe746bab9021bb5a97fa5cb9fdf61ad31624
Adson Souza <ty.msn@hotmail.com>
2021-06-26 21:42:58 -0300

    INicial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicacaoASP.Startup))]
+namespace AplicacaoASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
