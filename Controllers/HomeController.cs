eb3c61b4be51efd504ccd70036b39971ccfb47d3
Adson Souza <ty.msn@hotmail.com>
2021-06-27 23:31:54 -0300

    Criando models e enviando dados

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using AplicacaoASP.Models;
 using System.Web.Mvc;
 
 namespace AplicacaoASP.Controllers
@@ -10,6 +7,17 @@ namespace AplicacaoASP.Controllers
     {
         public ActionResult Index()
         {
+            var pessoa = new Pessoa();
+            pessoa.PessoaId = 1;
+            pessoa.Nome = "Adson Souza";
+            pessoa.Email = "ty.msn@hotmail.com";
+            pessoa.Tipo = "Física";
+
+            ViewData["PessoaId"] = pessoa.PessoaId;
+            ViewData["Nome"] = pessoa.Nome;
+            ViewData["Email"] = pessoa.Email;
+            ViewData["Tipo"] = pessoa.Tipo;
+
             return View();
         }
 
@@ -23,6 +31,17 @@ namespace AplicacaoASP.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult List(int PessoaId, string Nome, string Email, string Tipo)
+        {
+            ViewData["PessoaId"] = PessoaId;
+            ViewData["Nome"] = Nome;
+            ViewData["Email"] = Email;
+            ViewData["Tipo"] = Tipo;
 
             return View();
         }
---
+++ Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿namespace AplicacaoASP.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Tipo { get; set; }
+
+
+    }
+}
